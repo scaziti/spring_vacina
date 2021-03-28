3f2e9043be1ea638622a08ec015bdb3f9283b012
Raphael Scaziti <rscaziti@gmail.com>
2021-03-28 12:28:56 -0300

    feat(usuario/controller): adicionado método post para cadastro de usuário

--- src/main/java/controle/vacina/controller/UsuarioController.java
+++ src/main/java/controle/vacina/controller/UsuarioController.java
@@ -3,10 +3,13 @@ package controle.vacina.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +36,11 @@ public class UsuarioController
 		return this.repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<UsuarioModel> post(@RequestBody UsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
 	}
 }
