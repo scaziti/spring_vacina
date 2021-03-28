fe67a8a88fcd198a6975ad66759607800088278c
Raphael Scaziti <rscaziti@gmail.com>
2021-03-28 12:17:15 -0300

    feat(usuario/controller): adicionado class, além de métodos getall e getByID

---
+++ src/main/java/controle/vacina/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package controle.vacina.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import controle.vacina.model.UsuarioModel;
+import controle.vacina.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController 
+{
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> getAll()
+	{
+		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> getById(@PathVariable Long id)
+	{
+		return this.repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+}
