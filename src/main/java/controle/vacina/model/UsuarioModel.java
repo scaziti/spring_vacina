762e403402464199183e0af98899e0ea9124bb21
Raphael Scaziti <rscaziti@gmail.com>
2021-03-28 12:04:21 -0300

    feat(usuario/model): adicionado comentários de spring, além de atributos, getters e setters

--- src/main/java/controle/vacina/model/UsuarioModel.java
+++ src/main/java/controle/vacina/model/UsuarioModel.java
@@ -1,5 +1,91 @@
 package controle.vacina.model;
 
-public class UsuarioModel {
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_usuario")
+public class UsuarioModel 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 2)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 5)
+	private String email;
+	
+	@NotNull
+	@Size(min = 11)
+	private String cpf;
+	
+	@NotNull
+	@Size(min = 10)
+	private String nascimento;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
 }
